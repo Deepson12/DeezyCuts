9dae7e65b432ca8f4639ec38218c30b3d1cb3dc4
Epithet <deepsonsharma763976@gmail.com>
2026-01-03 16:39:07 +0545

    fix: fixed setState according to the value from sessionstorage

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,10 @@ import Preloader from '@/components/Preloader'
 import React, {  useEffect, useState } from 'react'
 
 const Page = () => {
-  const [loadPreLoader, setloadPreLoader] = useState(false);
+  const [loadPreLoader, setloadPreLoader] = useState(() =>
+  !!sessionStorage.getItem("preLoaderPlayed") ? false : true
+);
+    // const [checkDone, setCheckDone] = useState(false);
 
     useEffect(()=>{
       const hasPlayed = sessionStorage.getItem("preLoaderPlayed");
@@ -12,18 +15,24 @@ const Page = () => {
       if(!hasPlayed){
         requestAnimationFrame(() => {
         setloadPreLoader(true);
+        
       });
       }
+    
     },[])
 
-
+    //  if (!checkDone) return null;
   return (
     <div>
       {
         loadPreLoader && <Preloader onComplete={()=>setloadPreLoader(false)} />
       }
       
-      Hell
+      
+        <section>
+          Hell
+        </section>
+     
     </div>
   )
 }
